40079a3b57ff69c7e63d342d6303ce4dd540dc9a
manoj <manojreddy.a2@gmail.com>
2022-03-08 13:56:25 +0530

    removed comment

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let description = document.querySelector("#description");
 let temp = document.querySelector("#temp");
 let wind = document.querySelector("#wind");
 
-// http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e12fbca4dade4baef11d7d52ebf79424
 const apiKey = "e12fbca4dade4baef11d7d52ebf79424";
 
 // kelvin to celcius convert, 1 Kelvin is equal to -272.15 Celsius
